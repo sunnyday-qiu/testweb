62a27e097cdaf14b43f4edcc37aa0aeb32f8d5eb
changyeyu <1214210805@qq.com>
2021-07-29 16:24:03 +0800

    项目以war包形式打包

--- src/main/java/com/cyy/TestApplication.java
+++ src/main/java/com/cyy/TestApplication.java
@@ -3,10 +3,18 @@ package com.cyy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestApplication {
+public class TestApplication extends SpringBootServletInitializer {
     
+    
+    //覆写configure方法
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TestApplication.class);
+    }
     
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
